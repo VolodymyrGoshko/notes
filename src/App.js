50d43ffbb9ecbaa5ddfbbe3a49d15e24d027f5ea
VolodymyrGoshko <goshkovova771@gmail.com>
2021-01-29 23:15:16 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React  from 'react'
 import './App.css';
+import NotesList from "./Nodes/NotesList";
+import Context from "./context";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [notes, setNotes] = React.useState([
+        {id:1, completed:false, title:'Зробити дз з курсів'},
+        {id:2, completed:false, title:'Повчити англ мову'},
+        {id:3, completed:false, title:'Піти в магазин'}
+    ]);
+
+    function toggleNote(id) {
+        setNotes(notes.map(note => {
+            if (note.id === id) {
+                note.completed = !note.completed
+            }
+            return note
+        }))
+    }
+
+    function removeNote(id) {
+        setNotes(notes.filter(note => note.id !== id))
+    }
+
+    return (
+        <Context.Provider value={{removeNote}}>
+            <div className='wrapper'>
+                <h1 className='wrapper-title'>Notes</h1>
+                {notes.length ? <NotesList notes={notes} onToggle={toggleNote}/> : <p>No notes!</p>}
+            </div>
+        </Context.Provider>
+    );
 }
 
 export default App;
